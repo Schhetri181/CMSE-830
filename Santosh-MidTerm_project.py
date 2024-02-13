dac5bf06155bd3a109d82efd7f400cdfee6caa0c
Schhetri181 <143816426+Schhetri181@users.noreply.github.com>
2024-02-13 11:01:56 -0500

    Update Santosh-MidTerm_project.py

--- Santosh-MidTerm_project.py
+++ Santosh-MidTerm_project.py
@@ -15,7 +15,7 @@ st.title("Data Visualization on Drug use by different age group")
 image = "drug-use.png"
 st.image(image, caption="Picture credit- Internet", use_column_width=False, width=900)
 
-'''
+''''
 st.write(
     'Drug use seems like a consistent problem all across the US with no end in sight. Its a constant point of note'
     'in daily news.Substance misuse, including the abuse of prescription medications, illicit drugs, and alcohol, '
@@ -25,7 +25,7 @@ st.write(
     'demographics especially age. Through a thorough understanding of how drug use is different across various '
     'demographics we can create targeted programs to support people through their troubles of drug use.'
 )
-'''
+''''
 
 df = pd.read_csv("drug_use_by_age.csv")
 
